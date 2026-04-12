bc21991ddd4574b299a6c986e39f0293fcf57701
Ouchen224 <131014208+Ouchen224@users.noreply.github.com>
2026-04-12 20:16:23 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -83,12 +83,22 @@ const commands = [
 
 ].map(cmd => cmd.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-    await rest.put(
-        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-        { body: commands }
-    );
-    console.log("✅ Commandes enregistrées !");
+    try {
+        console.log("Déploiement des commandes...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            { body: commands }
+        );
+
+        console.log("✅ Commandes enregistrées !");
+    } catch (err) {
+        console.error("❌ Erreur :", err);
+    }
 })();
