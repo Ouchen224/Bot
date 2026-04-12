7f976ff038044bb7e2a23be2d726c0b0c275af81
Ouchen224 <131014208+Ouchen224@users.noreply.github.com>
2026-04-12 20:12:11 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -83,7 +83,7 @@ const commands = [
 
 ].map(cmd => cmd.toJSON());
 
-const rest = new REST({ version: '10' })..setToken(process.env.TOKEN)
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 
 (async () => {
     await rest.put(
